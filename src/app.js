0804c2de750e379368bcef478a5df4d178fd3630
amitdhiman5086 <amitdhiman5086@gmail.com>
2025-05-05 17:01:17 +0530

    Update CORS origin to use the correct backend URL

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["https://d-match.onrender.com", "http://localhost:5173"], // Allow requests from React app
+    origin: ["https://d-match-backend.onrender.com", "http://localhost:5173"], // Allow requests from React app
     credentials: true, // Allow cookies/auth
   })
 );
